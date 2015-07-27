c72c882d229563ae6181514af56d1cfb24c2ff44
Dan Patey <DPatey@telesign.com>
2015-07-27 16:12:31 -0700

    Three strikes and refactor

--- functional_tests.py
+++ functional_tests.py
@@ -25,7 +25,7 @@ class NewVisitorTest(unittest.TestCase):
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(
                 inputbox.get_attribute('placeholder'),
-                'enter a to-do item'
+                'Enter a to-do item'
         )
 
         # She types 'Buy peacock feathers' into a text box (Edith's hobby
@@ -38,9 +38,10 @@ class NewVisitorTest(unittest.TestCase):
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-                any(row.text == '1: Buy peacock feathers' for row in rows),
-                "New to-do item did not appear in table"
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn(
+            '2: Use peacock feathers to make a fly' ,
+             [row.text for row in rows]
         )
 
         # There is still a text box inviting her to add another item. She
